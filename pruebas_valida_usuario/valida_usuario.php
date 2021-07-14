c3e31ce407ce5e226258b1823532b869cf77b7ae
Josef Fodor <36340616+Enternovajfodor@users.noreply.github.com>
2021-07-14 10:53:35 -0500

    Delete pruebas_valida_usuario directory

--- pruebas_valida_usuario/valida_usuario.php
+++
@@ -1,8 +0,0 @@
-<? 
-   include("../librerias/lib/@include.php");
-define("SUE_PATH", "C:/Program Files/app_sgpa/sistem/");
-echo $us_local = $_SERVER["LOGON_USER"];
-
-//include(SUE_PATH."valida_ingreso_admin_local.php");
-
-?>
