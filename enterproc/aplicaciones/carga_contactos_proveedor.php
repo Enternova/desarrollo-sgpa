b683c22c45bf37c028f47def2fd4e3c78a64c7b9
Josef Fodor <36340616+Enternovajfodor@users.noreply.github.com>
2021-07-14 10:50:44 -0500

    Delete enterproc directory

--- enterproc/aplicaciones/carga_contactos_proveedor.php
+++
@@ -1,7 +0,0 @@
-<? 
-header('Content-Type: text/xml; charset=ISO-8859-1');
-echo '<?xml version="1.0" encoding="ISO-8859-1"?>';	
-
-readfile('http://www.parservicios.com/parservi/ficha_tecnica_gt.php?ref=principal.html&pv_nit='.$pv_nit);
-
-?>
